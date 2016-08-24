12aa61931e278d7ea980205d0dd375e74933fd8b
annadodd <annadodd@umich.edu>
2016-08-24 15:58:59 -0400

    Initial Commit

---
+++ Aug2016 Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Aug2016 Practice
+//
+//  Created by Anna Dodd on 8/24/16.
+//  Copyright © 2016 Anna Dodd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
